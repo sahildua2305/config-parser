f6f3fc457e61bad7d76c4c4de41fb545e6b0e419
Sahil Dua <sahil.dua@booking.com>
2018-11-22 00:12:30 +0100

    config: initial commit
    Initial commit containing the basic implementation for config parser.

---
+++ config.py
@@ -0,0 +1,387 @@
+"""
+Config Parser 
+"""
+
+import re
+
+# Compiled regular expression for matching group name.
+_GROUP_TMPL = r"""
+    \[      # [
+    (.+)   # group
+    \]      # ]
+    """
+GROUP_CRE = re.compile(_GROUP_TMPL, re.VERBOSE)
+
+# Compiled regular expression for matching (setting, value) pair.
+_SETTING_TMPL = r"""
+    ^(.+)    # setting
+    \s?       # optional space
+    =         # =
+    \s?       # optional space
+    (.+)$    # value
+    """
+SETTING_CRE = re.compile(_SETTING_TMPL, re.VERBOSE)
+
+# Compiled regular expression for matching setting override.
+_SETTING_OVERRIDE_TMPL = r"""
+    ^(.+)      # setting
+    <(.+)>     # <override>
+    \s?         # optional space
+    =           # =
+    \s?         # optional space
+    (.+)$      # value
+    """
+SETTING_OVERRIDE_CRE = re.compile(_SETTING_OVERRIDE_TMPL, re.VERBOSE)
+
+# Compiled regular expression for matching comments.
+_COMMENT_TMPL = r"""
+    ([;]+)    # ;
+    .*$       # comment text
+    """
+COMMENT_CRE = re.compile(_COMMENT_TMPL, re.VERBOSE)
+
+# Compiled regular expression for matching quoted strings.
+_QUOTED_STRING_TMPL = r"""
+    (\"|\')
+    (.+)
+    (\"|\')
+    """
+QUOTED_STRING_CRE = re.compile(_QUOTED_STRING_TMPL, re.VERBOSE)
+
+# Permitted boolean values for this config parser.
+PERMITTED_BOOLEAN_VALUES = {
+    "yes": True, "no": False,
+    "true": True, "false": False,
+    "1": True, "0": False,
+}
+
+
+class Error(Exception):
+    """Base class for custom exceptions.
+    """
+
+    def __init__(self, message):
+        super(Error, self).__init__(message)
+
+
+class DuplicateGroupError(Error):
+    """Custom error to return when a duplicate group is found
+    while parsing the configuration file.
+    """
+
+    def __init__(self, group, file_path, line_number):
+        message = "Duplicate group '" + str(group) + "' found at line " + \
+            str(line_number) + " while parsing file at " + str(file_path)
+        super(DuplicateGroupError, self).__init__(message)
+
+
+class InvalidLineError(Error):
+    """Custom error to return when a line can't be parsed
+    according to any of the known patterns.
+    """
+
+    def __init__(self, file_path, line_number):
+        message = "Unable to parse line " + \
+            str(line_number) + " while parsing file at " + str(file_path)
+        super(InvalidLineError, self).__init__(message)
+
+
+class MissingGroupError(Error):
+    """Custom error to return when the file doesn't start
+    with a group. This means we don't know how to handle the
+    settings in the beginning of the file.
+    """
+
+    def __init__(self, file_path, line_number):
+        message = "Unable to find a group at line " + \
+            str(line_number) + " while parsing file at " + str(file_path)
+        super(MissingGroupError, self).__init__(message)
+
+
+class AttributeDict(dict):
+    """AttributeDict is a wrapper around python dict
+    which makes it possible to access dictionary keys
+    as attributes.
+
+    We achieve this feature by overriding __getattr__ method
+    to return None when the key doesn't exist.
+    
+    We have also overridden __getitem__ method in order to
+    support returning None for non-existent keys when user is
+    trying to access the non-existent key as config["key"].
+
+    For example:
+    config["one"]["two"] can be accessed as: config.one.two
+    """
+
+    def __init__(self):
+        super(AttributeDict, self).__init__()
+    
+    def __getattr__(self, key):
+        return self.get(key, None)
+
+    def __getitem__(self, key):
+        return self.get(key, None)
+
+
+def read_file_lines(file_path):
+    """Function reads the file at given path and yields
+    lines from the file. This is a handy way to handle
+    reading big files line by line.
+
+    Returns a generator.
+    """
+    with open(file_path) as file:
+        for line in file:
+            yield line
+
+
+def is_empty_line(line):
+    """Function to check if the given line is whitespace only.
+
+    Returns a boolean.
+    """
+    if line and line.strip():
+        return False
+    return True
+
+
+def parse_group_name(line):
+    """Function to parse the group name from the following
+    pattern:
+
+    [group]
+
+    Returns a string.
+    """
+    match = GROUP_CRE.match(line)
+    if match is not None and len(match.groups()) == 1:
+        return match.group(1).strip()
+    return None
+
+
+def parse_setting_value(line):
+    """Function to parse the setting value from the following
+    pattern:
+
+    setting = value
+
+    Returns a tuple of (setting, value).
+    """
+    match = SETTING_CRE.match(line)
+    if match is not None and len(match.groups()) == 2:
+        return (
+            match.group(1).strip(),
+            parse_value(match.group(2).strip())
+        )
+    return None, None
+
+
+def parse_setting_override_value(line):
+    """Function to parse the setting override value from the following
+    pattern:
+
+    setting<override> = value
+
+    Returns a tuple of (setting, override, value).
+    """
+    match = SETTING_OVERRIDE_CRE.match(line)
+    if match is not None and len(match.groups()) == 3:
+        return (
+            match.group(1).strip(),
+            match.group(2).strip(),
+            parse_value(match.group(3).strip())
+        )
+    return (None, None, None)
+
+
+def trim_comment(line):
+    """Function to trim off the comment starting with ';'
+    character from the given line. A typical comment looks
+    like this:
+
+    ; this is a comment
+    or
+    [group]; this is a comment
+
+    Returns the line after trimming the comment, if any.
+    """
+    return COMMENT_CRE.sub(repl="", string=line).strip()
+
+
+def is_number(s):
+    """Function to check if the string is a number.
+
+    Returns True if string is a number.
+    """
+    return s.replace('.', '', 1).isdigit()
+
+
+def get_int(s):
+    """Function to return int value from the string, if
+    it's a valid one.
+    """
+    try:
+        return int(s)
+    except ValueError:
+        return None
+
+
+def get_float(s):
+    """Function to return float value from the string, if
+    it's a valid one.
+    """
+    try:
+        return float(s)
+    except ValueError:
+        return None
+
+
+def get_boolean(s):
+    """Function to return boolean value from the string, if
+    it's a valid one according to the permitted boolean values.
+    """
+    if s.lower() in PERMITTED_BOOLEAN_VALUES:
+        return PERMITTED_BOOLEAN_VALUES[s]
+    return None
+
+
+def get_list(s):
+    """Function to return list of strings, if the given string
+    is a comma separated list of values.
+    """
+    list_value = s.split(",")
+    if len(list_value) > 1:
+        return [parse_value(element.strip()) for element in list_value]
+    return None
+
+
+def get_quoted_string(s):
+    """Function to return quoted string.
+    """
+    quoted_string = QUOTED_STRING_CRE.match(s)
+    if quoted_string is not None and len(quoted_string.groups()) == 3:
+        return quoted_string.group(2).strip()
+    return None
+
+
+def parse_value(value):
+    """Function to parse a setting value.
+    The string parsed can be any of the following types:
+
+    int
+    boolean
+    array of comma separated values
+    string
+    string wrapped in quotes (single and double)
+    """
+    # Check for quoted string
+    quoted_string = get_quoted_string(value)
+    if quoted_string is not None:
+        return quoted_string
+
+    # If the value looks like a number, check for both
+    # int and float in greedy fashion.
+    if is_number(value):
+        # Check for integer
+        integer_value = get_int(value)
+        if integer_value is not None:
+            return integer_value
+
+        # Check for float
+        float_value = get_float(value)
+        if float_value is not None:
+            return float_value
+
+    # Check for boolean
+    boolean_value = get_boolean(value)
+    if boolean_value is not None:
+        return boolean_value
+
+    # Check for list of values
+    list_value = get_list(value)
+    if list_value is not None:
+        return list_value
+
+    return value
+
+
+def load_config(file_path, overrides=None):
+    """Main function to parse a configuration file from a given
+    file path and a list of overrides.
+    """
+    # Initialize config as AttributeDict.
+    config = AttributeDict()
+    # Keeping track of line number here to be used for error reporting.
+    line_number = 0
+    # Keeping track of current group here to be used to save setting, value pairs.
+    curr_group = None
+
+    # Convert overrides into a set so that we can easily look up
+    # if a given override is enabled or not.
+    enabled_overrides = set()
+    if overrides is not None:
+        enabled_overrides = set(overrides)
+
+    for line in read_file_lines(file_path):
+        line_number += 1
+
+        # Trim off the inline comment from end of the line.
+        line = trim_comment(line)
+
+        # Skip the empty lines.
+        # Here, we are also covering the line which contains a comment
+        # only. For such a line, we will be left with empty string after
+        # trimming the comment on last line.
+        if is_empty_line(line):
+            continue
+
+        # Try to parse group name from current line.
+        new_group = parse_group_name(line)
+        if new_group is not None:
+            # If we have seen this group before, raise exception.
+            #
+            # IDEA: We have two alternate options here.
+            # - we can overwrite the group settings if we find it again.
+            # - we can ignore if a group is found as a duplicate.
+            if new_group in config:
+                raise DuplicateGroupError(new_group, file_path, line_number)
+
+            # Initialize a new AttributeDict since this is a new group.
+            config[new_group] = AttributeDict()
+            # Update current group to which we will be saving all next settings.
+            curr_group = new_group
+            continue
+
+        # Try to parse (setting, value) pair from current line.
+        original_setting, value = parse_setting_value(line)
+        if original_setting is not None and value is not None:
+            # If we found a settings line, however, there was no group
+            # found before while parsing this file, raise exception.
+            #
+            # IDEA: This scenario is up to us how we want to handle it.
+            # Alternatively, we could also simply ignore all settings
+            # until we find a group in the file.
+            if curr_group is None:
+                raise MissingGroupError(file_path, line_number)
+
+            # Try to parse according to setting override pattern.
+            setting_without_override, override, __ = parse_setting_override_value(line)
+            if override is None: # no override found
+                config[curr_group][original_setting] = value
+            elif override in enabled_overrides: # an enabled override found
+                config[curr_group][setting_without_override] = value
+            continue
+
+        # If we reach this point, that means we weren't able to parse
+        # the current line in any of the known ways.
+        #
+        # IDEA: This decision is up to us how we want to handle it.
+        # Alternatively, we could also simply ignore any line that we don't
+        # identify and keep on reading the file further.
+        raise InvalidLineError(file_path, line_number)
+    return config
+
+if __name__ == "__main__":
+    CONFIG = load_config("./config_data/sample_config.conf", overrides=['ubuntu', 'production'])
+    print CONFIG
---
+++ test_config.py
@@ -0,0 +1,294 @@
+# Run with: `python -m unittest discover`
+
+import unittest
+import config
+
+
+class TestGetInt(unittest.TestCase):
+    """Class to test `get_int` method.
+    """
+
+    def test_valid_int_value(self):
+        s = "1"
+        self.assertEqual(config.get_int(s), 1)
+
+    def test_invalid_int_float_value(self):
+        s = "1.00"
+        self.assertIsNone(config.get_int(s))
+
+    def test_invalid_int_value(self):
+        s = "as"
+        self.assertIsNone(config.get_int(s))
+
+
+class TestGetFloat(unittest.TestCase):
+    """Class to test `get_float` method.
+    """
+
+    def test_valid_float_value(self):
+        s = "1.00"
+        self.assertEqual(config.get_float(s), 1.00)
+
+    def test_valid_float_int_value(self):
+        s = "1"
+        self.assertEqual(config.get_float(s), 1)
+
+    def test_invalid_float_value(self):
+        s = "1.2s"
+        self.assertIsNone(config.get_float(s))
+
+
+class TestGetBoolean(unittest.TestCase):
+    """Class to test `get_boolean` method.
+    """
+
+    def test_valid_true_boolean_value(self):
+        s = "true"
+        self.assertTrue(config.get_boolean(s))
+
+    def test_valid_zero_boolean_value(self):
+        s = "0"
+        self.assertFalse(config.get_boolean(s))
+
+    def test_valid_no_boolean_value(self):
+        s = "no"
+        self.assertFalse(config.get_boolean(s))
+
+    def test_invalid_boolean_value_string(self):
+        s = "what"
+        self.assertIsNone(config.get_boolean(s))
+
+    def test_invalid_boolean_value_list(self):
+        s = "no,yes"
+        self.assertIsNone(config.get_boolean(s))
+
+
+class TestGetList(unittest.TestCase):
+    """Class to test `get_list` method.
+    """
+
+    def test_valid_list_without_space(self):
+        s = "array,of,values"
+        self.assertListEqual(
+            config.get_list(s),
+            ["array", "of", "values"]
+        )
+
+    def test_valid_list_with_space(self):
+        s = "array, of, values"
+        self.assertListEqual(
+            config.get_list(s),
+            ["array", "of", "values"]
+        )
+
+    def test_valid_list_of_booleans(self):
+        s = "1,true, no"
+        self.assertListEqual(
+            config.get_list(s),
+            [True, True, False]
+        )
+
+    def test_valid_list_of_int_floats(self):
+        s = "1.0, 2, 3.3"
+        self.assertListEqual(
+            config.get_list(s),
+            [1.0, 2, 3.3]
+        )
+
+    def test_invalid_list_string(self):
+        s = "word"
+        self.assertIsNone(config.get_list(s))
+
+class TestTrimComment(unittest.TestCase):
+    """Class to test `trim_comment` method.
+    """
+
+    def test_trim_inline_comment(self):
+        line = "path = /tmp/; comment"
+        self.assertEqual(config.trim_comment(line), "path = /tmp/")
+
+    def test_trim_full_line_comment(self):
+        line = "; comment line"
+        self.assertEqual(config.trim_comment(line), "")
+
+    def test_trim_full_line_comment_with_leading_spaces(self):
+        line = "                ;      comment line"
+        self.assertEqual(config.trim_comment(line), "")
+
+
+class TestParseGroupName(unittest.TestCase):
+    """Class to test `parse_group_name` method.
+    """
+
+    def test_valid_group_name(self):
+        line = "[http]"
+        self.assertEqual(config.parse_group_name(line), "http")
+
+    def test_invalid_group_name(self):
+        line = "http"
+        self.assertIsNone(config.parse_group_name(line))
+
+    def test_empty_group_name(self):
+        line = "[]"
+        self.assertIsNone(config.parse_group_name(line))
+
+
+class TestParseSettingValue(unittest.TestCase):
+    """Class to test `parse_setting_value` method.
+    """
+
+    def test_invalid_setting_value(self):
+        line = "[group]"
+        self.assertTupleEqual(
+            config.parse_setting_value(line),
+            (None, None)
+        )
+
+    def test_valid_setting_value_with_space(self):
+        line = "path = /tmp/"
+        self.assertTupleEqual(
+            config.parse_setting_value(line),
+            ("path", "/tmp/")
+        )
+
+    def test_valid_setting_value_without_space(self):
+        line = "path=/tmp/"
+        self.assertTupleEqual(
+            config.parse_setting_value(line),
+            ("path", "/tmp/")
+        )
+
+    def test_empty_string(self):
+        line = "=1"
+        self.assertTupleEqual(
+            config.parse_setting_value(line),
+            (None, None)
+        )
+    
+    def test_valid_setting_value_with_override(self):
+        line = "path<staging> = /srv/uploads/"
+        self.assertTupleEqual(
+            config.parse_setting_value(line),
+            ("path<staging>", "/srv/uploads/")
+        )
+
+    def test_valid_setting_int_value(self):
+        line = "basic_size_limit = 26214400"
+        self.assertTupleEqual(
+            config.parse_setting_value(line),
+            ("basic_size_limit", 26214400)
+        )
+
+    def test_valid_setting_boolean_value(self):
+        line = "enabled = no"
+        self.assertTupleEqual(
+            config.parse_setting_value(line),
+            ("enabled", False)
+        )
+
+    def test_valid_list_value_without_space(self):
+        line = "params = array,of,values"
+        self.assertTupleEqual(
+            config.parse_setting_value(line),
+            ("params", ["array", "of", "values"])
+        )
+
+    def test_valid_list_value_with_space(self):
+        line = "params = array, of, values"
+        self.assertTupleEqual(
+            config.parse_setting_value(line),
+            ("params", ["array", "of", "values"])
+        )
+
+
+class TestParseSettingOverrideValue(unittest.TestCase):
+    """Class to test `parse_setting_override_value` method.
+    """
+
+    def test_valid_setting_override_value(self):
+        line = "path<production> = /srv/var/tmp/"
+        self.assertTupleEqual(
+            config.parse_setting_override_value(line),
+            ("path", "production", "/srv/var/tmp/")
+        )
+
+    def test_invalid_setting_override_value(self):
+        line = "path = /srv/var/tmp/"
+        self.assertTupleEqual(
+            config.parse_setting_override_value(line),
+            (None, None, None)
+        )
+
+
+class TestLoadConfig(unittest.TestCase):
+    """Class to test `load_config` method.
+    """
+
+    def test_valid_small_config_without_overrides(self):
+        CONFIG = config.load_config("./test_config_data/config_small.conf")
+
+        # Check for type of CONFIG
+        self.assertIsNotNone(CONFIG)
+        self.assertIsInstance(CONFIG, dict)
+
+        # Check for invalid keys
+        self.assertIsNone(CONFIG.something)
+        self.assertIsNone(CONFIG.http.something)
+
+        # Check for some valid settings
+        self.assertEqual(CONFIG.common.student_size_limit, 52428800)
+        self.assertListEqual(CONFIG.http.params, ["array", "of", "values"])
+        self.assertEqual(CONFIG.ftp.name, "hello there, ftp uploading")
+        self.assertDictEqual(
+            CONFIG.ftp,
+            {
+                'path': '/tmp/',
+                'enabled': False,
+                'name': 'hello there, ftp uploading'
+            }
+        )
+
+    def test_valid_small_config_with_overrides(self):
+        CONFIG = config.load_config("./test_config_data/config_small.conf", overrides=["production", "ubuntu"])
+
+        # Check for type of CONFIG
+        self.assertIsNotNone(CONFIG)
+        self.assertIsInstance(CONFIG, dict)
+
+        # Check for invalid settings
+        self.assertIsNone(CONFIG.something)
+        self.assertIsNone(CONFIG["something"])
+        self.assertIsNone(CONFIG.http.something)
+
+        # Check for some valid settings
+        self.assertEqual(CONFIG.common.student_size_limit, 52428800)
+        self.assertEqual(CONFIG.common["student_size_limit"], 52428800)
+        self.assertListEqual(CONFIG.http.params, ["array", "of", "values"])
+        self.assertEqual(CONFIG.ftp.name, "hello there, ftp uploading")
+        self.assertDictEqual(
+            CONFIG.ftp,
+            {
+                'path': '/etc/var/uploads',
+                'enabled': False,
+                'name': 'hello there, ftp uploading'
+            }
+        )
+
+        # Check for some settings with overrides
+        self.assertEqual(CONFIG.ftp.path, "/etc/var/uploads")
+        self.assertEqual(CONFIG.ftp["path"], "/etc/var/uploads")
+
+    def test_invalid_config_with_missing_group(self):
+        with self.assertRaises(config.MissingGroupError):
+            ___ = config.load_config("./test_config_data/config_missing_group.conf")
+
+    def test_invalid_config_with_duplicate_group(self):
+        with self.assertRaises(config.DuplicateGroupError):
+            __ = config.load_config("./test_config_data/config_duplicate_group.conf")
+
+    def test_invalid_config_with_garbage_line(self):
+        with self.assertRaises(config.InvalidLineError):
+            __ = config.load_config("./test_config_data/config_garbage_line.conf")
+
+if __name__ == "__main__":
+    unittest.main()
